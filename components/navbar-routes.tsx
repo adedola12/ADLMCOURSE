2c0153399f8631ff560877d24b750ba2abe4f756
Adedolapo <dolapo836@gmail.com>
2023-10-10 06:18:04 +0100

    final

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
 import { usePathname, useRouter } from "next/navigation";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "./ui/button";
 import { SearchInput } from "./search-input";
+import { isTeacher } from "@/lib/teacher";
 
 export const NavbarRoutes = () => {
+  const { userId } = useAuth()
+
   const pathname = usePathname();
-  const router = useRouter();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isCoursePage = pathname?.includes("/courses");
@@ -31,13 +33,13 @@ export const NavbarRoutes = () => {
             Exit
           </Button>
         </Link>
-      ) : (
+      ) : isTeacher(userId) ? (
         <Link href="/teacher/courses">
           <Button size="sm" variant="ghost">
             Teacher mode
           </Button>
         </Link>
-      )}
+      ) : null }
       <UserButton afterSignOutUrl="/" />
     </div>
     </>
